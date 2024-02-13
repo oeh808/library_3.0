fc7edec660ebb9f8c535ea0f8e22542c15813818
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-13 10:02:45 +0200

    Added enum for book categories

---
+++ src/main/java/io/library/library_3/enums/Category.java
@@ -0,0 +1,13 @@
+package io.library.library_3.enums;
+
+public enum Category {
+    FICTION,
+    NONFICTION,
+    ACTION,
+    COMEDY,
+    MYSTERY,
+    DRAMA,
+    ROMANCE,
+    HORROR,
+    FANTASY
+}
