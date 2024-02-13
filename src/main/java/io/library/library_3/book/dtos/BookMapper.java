4dd7bddffc81e5785aac9fa89a2173c588498922
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-13 13:10:22 +0200

    Split conversion of enum aray to seperate methods to allow reuse

--- src/main/java/io/library/library_3/book/dtos/BookMapper.java
+++ src/main/java/io/library/library_3/book/dtos/BookMapper.java
@@ -9,33 +9,40 @@ import io.library.library_3.enums.Category;
 public class BookMapper {
     // To DTO
     public BookCreationDTO toDTO(Book book) {
-        String[] categs = book.getCategories();
-        Category[] categories = new Category[categs.length];
-        for (int i = 0; i < categs.length; i++) {
-            categories[i] = Category.valueOf(categs[i]);
-        }
-
         BookCreationDTO dto = new BookCreationDTO();
         dto.setTitle(book.getTitle());
         dto.setNumOfPages(book.getNumOfPages());
         dto.setQuantity(book.getQuantity());
         dto.setAuthors(book.getAuthors());
-        dto.setCategories(categories);
+        dto.setCategories(toCategories(book.getCategories()));
 
         return dto;
     }
 
     // To entity
     public Book toBook(BookCreationDTO dto) {
-        Category[] categs = dto.getCategories();
+        Book book = new Book(dto.getTitle(), dto.getAuthors(),
+                dto.getNumOfPages(), dto.getQuantity(), fromCategories(dto.getCategories()));
+
+        return book;
+    }
+
+    // Custom
+    // __________________________________________________
+    public String[] fromCategories(Category[] categs) {
         String[] categories = new String[categs.length];
         for (int i = 0; i < categs.length; i++) {
             categories[i] = categs[i].toString();
         }
 
-        Book book = new Book(dto.getTitle(), dto.getAuthors(),
-                dto.getNumOfPages(), dto.getQuantity(), categories);
+        return categories;
+    }
 
-        return book;
+    public Category[] toCategories(String[] categs) {
+        Category[] categories = new Category[categs.length];
+        for (int i = 0; i < categs.length; i++) {
+            categories[i] = Category.valueOf(categs[i]);
+        }
+        return categories;
     }
 }
