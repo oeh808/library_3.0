1ff1fa49d74ef8a9731c1fe89288afbfd1e454dc
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-13 16:01:53 +0200

    Removed unnecessary table creation

--- src/main/java/io/library/library_3/user/entity/User.java
+++ src/main/java/io/library/library_3/user/entity/User.java
@@ -11,10 +11,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Users")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class User {
     @Id
