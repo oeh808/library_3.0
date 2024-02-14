42d9012ae889a08034cbb39eeec6f950ede2d43c
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-14 10:46:18 +0200

    Added custom query for retrieving distinct books

--- src/main/java/io/library/library_3/borrowed_book/repo/BorrowedBookRepo.java
+++ src/main/java/io/library/library_3/borrowed_book/repo/BorrowedBookRepo.java
@@ -1,9 +1,14 @@
 package io.library.library_3.borrowed_book.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import io.library.library_3.book.entity.Book;
 import io.library.library_3.borrowed_book.entity.BorrowedBook;
 
 public interface BorrowedBookRepo extends JpaRepository<BorrowedBook, Integer> {
-
+    @Query("SELECT DISTINCT bb.book FROM BorrowedBook bb")
+    public List<Book> findAllBooks();
 }
