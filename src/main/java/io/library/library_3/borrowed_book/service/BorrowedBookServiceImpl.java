ef1b3f4607a15811fb67a4385e137ee9167d1611
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-14 10:47:07 +0200

    Updated skeleton of Borrowed Book Service's implementation

--- src/main/java/io/library/library_3/borrowed_book/service/BorrowedBookServiceImpl.java
+++ src/main/java/io/library/library_3/borrowed_book/service/BorrowedBookServiceImpl.java
@@ -2,26 +2,45 @@ package io.library.library_3.borrowed_book.service;
 
 import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
 
+import org.hibernate.usertype.UserType;
 import org.springframework.stereotype.Service;
 
 import io.library.library_3.book.entity.Book;
+import io.library.library_3.book.repo.BookRepo;
 import io.library.library_3.borrowed_book.entity.BorrowedBook;
+import io.library.library_3.borrowed_book.repo.BorrowedBookRepo;
+import io.library.library_3.librarian.entity.Librarian;
+import io.library.library_3.librarian.repo.LibrarianRepo;
+import io.library.library_3.student.entity.Student;
+import io.library.library_3.student.repo.StudentRepo;
 import io.library.library_3.user.entity.User;
 
 @Service
 public class BorrowedBookServiceImpl implements BorrowedBookService {
+    private BorrowedBookRepo borrowedBookRepo;
+    private LibrarianRepo librarianRepo;
+    private StudentRepo studentRepo;
+    private BookRepo bookRepo;
+
+    public BorrowedBookServiceImpl(BorrowedBookRepo borrowedBookRepo, LibrarianRepo librarianRepo,
+            StudentRepo studentRepo, BookRepo bookRepo) {
+        this.borrowedBookRepo = borrowedBookRepo;
+        this.librarianRepo = librarianRepo;
+        this.studentRepo = studentRepo;
+        this.bookRepo = bookRepo;
+    }
 
     @Override
-    public void borrowBook(String refId, int userId, Date dateDue) {
+    public void borrowBook(String refId, int userId, Date dateDue, @SuppressWarnings("rawtypes") UserType userType) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'borrowBook'");
     }
 
     @Override
     public List<Book> getBooksBorrowed() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getBooksBorrowed'");
+        return borrowedBookRepo.findAllBooks();
     }
 
     @Override
@@ -31,19 +50,20 @@ public class BorrowedBookServiceImpl implements BorrowedBookService {
     }
 
     @Override
-    public List<Book> getBooksBorrowedByUser(int userId) {
+    public List<Book> getBooksBorrowedByUser(int userId, @SuppressWarnings("rawtypes") UserType userType) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getBooksBorrowedByUser'");
     }
 
     @Override
-    public BorrowedBook updateBorrowedBookDate(BorrowedBook borrowedBook) {
+    public BorrowedBook updateBorrowedBookDate(BorrowedBook borrowedBook,
+            @SuppressWarnings("rawtypes") UserType userType) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'updateBorrowedBookDate'");
     }
 
     @Override
-    public void returnBook(int id) {
+    public void returnBook(int id, @SuppressWarnings("rawtypes") UserType userType) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'returnBook'");
     }
