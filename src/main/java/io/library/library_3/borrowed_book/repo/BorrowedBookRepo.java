9ba8a97e741725b4b42bfada5aac1d8edf83c4e8
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-14 11:52:09 +0200

    Added custom queries to get users borrowing a book

--- src/main/java/io/library/library_3/borrowed_book/repo/BorrowedBookRepo.java
+++ src/main/java/io/library/library_3/borrowed_book/repo/BorrowedBookRepo.java
@@ -4,11 +4,17 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import io.library.library_3.book.entity.Book;
 import io.library.library_3.borrowed_book.entity.BorrowedBook;
+import io.library.library_3.user.entity.User;
 
 public interface BorrowedBookRepo extends JpaRepository<BorrowedBook, Integer> {
     @Query("SELECT DISTINCT bb.book FROM BorrowedBook bb")
     public List<Book> findAllBooks();
+
+    @Query("SELECT bb.user FROM BorrowedBook bb " +
+            "WHERE bb.book = :book")
+    public List<User> findUsersBorrowingBook(@Param("book") Book book);
 }
