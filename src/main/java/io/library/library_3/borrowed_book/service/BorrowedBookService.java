efe95c4828ef44ea5d970b5c49278e20d129e9d3
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-14 10:46:39 +0200

    Added a UserType enum to reduce amount of searching necessary

--- src/main/java/io/library/library_3/borrowed_book/service/BorrowedBookService.java
+++ src/main/java/io/library/library_3/borrowed_book/service/BorrowedBookService.java
@@ -3,13 +3,15 @@ package io.library.library_3.borrowed_book.service;
 import java.sql.Date;
 import java.util.List;
 
+import org.hibernate.usertype.UserType;
+
 import io.library.library_3.book.entity.Book;
 import io.library.library_3.borrowed_book.entity.BorrowedBook;
 import io.library.library_3.user.entity.User;
 
 public interface BorrowedBookService {
     // Create
-    public void borrowBook(String refId, int userId, Date dateDue);
+    public void borrowBook(String refId, int userId, Date dateDue, @SuppressWarnings("rawtypes") UserType userType);
 
     // Read
     // No need to retrieve borrowed books with BorrowedBook type
@@ -17,11 +19,12 @@ public interface BorrowedBookService {
 
     public List<User> getUsersBorrowingBook(String refId);
 
-    public List<Book> getBooksBorrowedByUser(int userId);
+    public List<Book> getBooksBorrowedByUser(int userId, @SuppressWarnings("rawtypes") UserType userType);
 
     // Update
-    public BorrowedBook updateBorrowedBookDate(BorrowedBook borrowedBook);
+    public BorrowedBook updateBorrowedBookDate(BorrowedBook borrowedBook,
+            @SuppressWarnings("rawtypes") UserType userType);
 
     // Delete
-    public void returnBook(int id);
+    public void returnBook(int id, @SuppressWarnings("rawtypes") UserType userType);
 }
---
+++ src/main/java/io/library/library_3/enums/UserType.java
@@ -0,0 +1,6 @@
+package io.library.library_3.enums;
+
+public enum UserType {
+    STUDENT,
+    LIBRARIAN
+}
