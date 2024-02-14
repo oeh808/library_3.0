2ca665a8c27c1de1c02f01c56c9fcf418eabb7d3
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-14 13:59:54 +0200

    Added error messages for unregistered students comitting unauthorized actions

--- src/main/java/io/library/library_3/student/StudentExceptionMessages.java
+++ src/main/java/io/library/library_3/student/StudentExceptionMessages.java
@@ -1,6 +1,8 @@
 package io.library.library_3.student;
 
 public class StudentExceptionMessages {
+    public static String NOT_REGISTERED = "You cannot go through with this action until your registeration is approved.";
+
     public static String ID_NOT_FOUND(int id) {
         return ("A student with id = " + id + " does not exist.");
     }
