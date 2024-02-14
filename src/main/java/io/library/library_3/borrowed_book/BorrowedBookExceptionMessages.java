56423072db884e3611761bcb65dd1aa2d0eb9132
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-14 12:17:10 +0200

    Added exception message for borrowed book

--- src/main/java/io/library/library_3/borrowed_book/BorrowedBookExceptionMessages.java
+++ src/main/java/io/library/library_3/borrowed_book/BorrowedBookExceptionMessages.java
@@ -1,5 +1,7 @@
 package io.library.library_3.borrowed_book;
 
 public class BorrowedBookExceptionMessages {
-
+    public static String ID_NOT_FOUND(int id) {
+        return ("A borrowed book with id = " + id + " does not exist.");
+    }
 }
