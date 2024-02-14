a041c9e72cb10e73b4f129cace0ed033a38e2de5
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-14 09:24:01 +0200

    Cleaned up comments

--- src/main/java/io/library/library_3/borrowed_book/dtos/BorrowedBookReadingDTO.java
+++ src/main/java/io/library/library_3/borrowed_book/dtos/BorrowedBookReadingDTO.java
@@ -3,15 +3,8 @@ package io.library.library_3.borrowed_book.dtos;
 import java.sql.Date;
 
 public class BorrowedBookReadingDTO {
-    // @NotBlank(message = "A borrowed book must have a book's reference id
-    // associated with it.")
     private String refId;
-    // @NotNull(message = "A borrowed book must have a user's id associated with
-    // it.")
-    // @Positive(message = "A user's id must be more than 0.")
     private int userId;
-    // @Future(message = "Provided due date for returning the book must be in the
-    // future.")
     private Date dateDue;
 
     public BorrowedBookReadingDTO() {
