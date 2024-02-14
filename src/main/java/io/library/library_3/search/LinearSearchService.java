647573b97311c2a714635a9ed7646ca91150b50c
OEH808 <omarehababdelaziz8@gmail.com>
2024-02-14 12:48:31 +0200

    Updated search service to be case insensitive

--- src/main/java/io/library/library_3/search/LinearSearchService.java
+++ src/main/java/io/library/library_3/search/LinearSearchService.java
@@ -2,6 +2,8 @@ package io.library.library_3.search;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,14 @@ public class LinearSearchService implements SearchService {
         if (vals.length == 0)
             return true;
 
-        HashSet<String> valuesToFind = new HashSet<String>(Arrays.asList(vals));
+        HashSet<String> valuesToFind = new HashSet<String>();
+        Set<String> temp = Arrays.asList(vals).stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet());
+        valuesToFind.addAll(temp);
+
         for (String str : arr) {
+            str = str.toLowerCase();
             if (valuesToFind.size() == 0)
                 break;
             if (valuesToFind.contains(str)) {
